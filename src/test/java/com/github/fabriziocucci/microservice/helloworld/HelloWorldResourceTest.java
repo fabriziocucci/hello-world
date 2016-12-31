62a1aeb8cf9d6149217d229ec0963ef9fe739a73
Fabrizio Cucci <fabrizio.cucci@gmail.com>
2016-12-31 17:28:18 +0100

    Added failing test

---
+++ src/test/java/com/github/fabriziocucci/microservice/helloworld/HelloWorldResourceTest.java
@@ -0,0 +1,23 @@
+package com.github.fabriziocucci.microservice.helloworld;
+
+import static org.junit.Assert.*;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+public class HelloWorldResourceTest extends JerseyTest {
+
+	@Override
+	protected Application configure() {
+		return new HelloWorldApplication();
+	}
+	
+	@Test
+	public void test() {
+		String response = target("/hello-world").request().get(String.class);
+		assertEquals("Hello World?", response); // I want to see the test fail!
+	}
+
+}
